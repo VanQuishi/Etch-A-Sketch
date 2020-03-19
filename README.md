c50b3b125258a99d6f81c3bc13460e7fe9c2e0d7
Chau Nguyen <songchau99@gmail.com>
2020-03-19 14:05:00 -0500

    Brainstorm on methods to use to create grids

---
+++ README.md
@@ -0,0 +1,5 @@
+This project implements Etch-A-Sketch by maniplating hover effect
+
+This is a JavaScript project from the Odin curriculum: https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project
+
+This is an ongoing project...
---
+++ interaction.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+
+/*create a function makeGrid(size): size is number of squares per side
+    have a for loop that create div belongs to class "grid-item"
+                        append the grid -item div to the container div
+
+*/
