b9a9cd27372a8720bdeb36cd6285fc1d18225771
Chau Nguyen <songchau99@gmail.com>
2020-03-21 22:16:40 -0500

    Create a square div with default value of 16x16

--- interaction.js
+++ interaction.js
@@ -5,3 +5,21 @@ const container = document.querySelector('.container');
                         append the grid -item div to the container div
 
 */
+
+function makeGrid(size = 16) {
+    container.style.setProperty('--grid-rows', size);
+    container.style.setProperty('--grid-cols', size);
+
+    let squares = size*size;
+
+    for (let i = 0; i < squares; i++) 
+    {
+        let gridItem = document.createElement('div');
+        gridItem.classList.add('gridItem');
+        container.appendChild(gridItem);
+    }
+
+}
+
+//makeGrid(25);
+makeGrid();
